39e87dc60b0f2d4bddff4ea8086ea479a98f22dd
Jacob Gadikian <jacobgadikian@gmail.com>
2019-09-09 14:05:39 -0400

    Update setupvalitdator.sh
    Hopefully this will now correctly assign the path for the go stuff to run from the start

--- setupvalitdator.sh
+++ setupvalitdator.sh
@@ -5,8 +5,8 @@ sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt-get update
 sudo apt-get install golang-go build-essential
 mkdir -p $HOME/go/bin
-echo "export GOPATH=$HOME/go" >> ~/.bash_profile
-echo "export PATH=\$PATH:\$GOPATH/bin" >> ~/.bash_profile
+echo 'export GOPATH=~/go' >> ~/.bash_profile
+echo 'export PATH=$PATH:~go/bin' >> ~/.bash_profile
 source ~/.bash_profile
 
 #Clones and installs Tendermint
