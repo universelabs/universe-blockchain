bab1c183dd89f37eaba976e8dc20888a2001bad5
Jacob Gadikian <jacobgadikian@gmail.com>
2019-09-09 14:14:13 -0400

    Update setupvalitdator.sh

--- setupvalitdator.sh
+++ setupvalitdator.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install golang-go build-essential
 mkdir -p $HOME/go/bin
 echo 'export GOPATH=~/go' >> ~/.bash_profile
-echo 'export PATH=$PATH:~go/bin' >> ~/.bash_profile
+echo 'export PATH=$PATH:~/go/bin' >> ~/.bash_profile
 source ~/.bash_profile
 
 #Clones and installs Tendermint
