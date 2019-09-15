4cfe1c7b634c10df1d550af28c07abf1536b1a51
Jacob Gadikian <jacobgadikian@gmail.com>
2019-09-15 10:32:26 -0400

    Update uninstall-mac.sh
    added y/n, improved greeting and thank user

--- uninstall-mac.sh
+++ uninstall-mac.sh
@@ -1,7 +1,13 @@
-echo -n "Would you like to remove the Universe Blockchain from your computer?"
+read -p "Would you like to remove the Universe Blockchain from your computer? (Y/n)(: " choice
 
+case "$choice" in
+    y|Y) echo -e "Uninstalling";;
+    *) echo "Aborting."; exit 1;;
+esac
 
 # UNINSTALLS UNIVERSE
 rm -rf ~/.universe
 rm -rf ~/.gaiad
 rm -rf ~/.gaiacli
+
+echo "Universe has been successfully Uninstalled.  Thank you!"
